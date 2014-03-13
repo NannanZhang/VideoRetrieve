a07efe1a948a52c3ff6e5b2ab58cf5dda1611059
NannanZhang <nannanbupt@gmail.com>
2014-03-13 08:38:45 +0800

    utils.h

---
+++ utils.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <string>
+#include <vector>
+//#include <windows.h>
+#include <stdlib.h>
+#include <map>
+
+using namespace std;
+std::vector<std::string> getfilelist(std::string strSourceDir);
+
+
